60799c4f9879ff5a64dcea0ecb8c16d9c44248cf
Robson santos <rcflorestal@yahoo.com.br>
2023-02-20 20:44:42 -0300

    Update file paths

--- umf_pa_interactive_map.R
+++ umf_pa_interactive_map.R
@@ -3,14 +3,14 @@ library(sf, warn.conflicts = FALSE)
 library(tmap, warn.conflicts = FALSE)
 
 umf <- st_read(
-        'D:/concessaoFlorestal/data/umf_concessao_pa.shp'
+        'D:/concessaoFlorestal/data/umf_concessao_pa/umf_concessao_pa.shp'
 ) %>%
         mutate(hectares = as.numeric(hectares))
 
 
 tmap_mode('view')
 
-tm_shape(umf) +
+umf_map <- tm_shape(umf) +
         tm_polygons(col = NA, 
                     border.col = 'red', 
                     alpha = 0.5,
