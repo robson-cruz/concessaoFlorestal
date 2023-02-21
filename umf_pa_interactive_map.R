92ef66df4c89869aa62ce015c5ea2f5a818621bb
Robson santos <rcflorestal@yahoo.com.br>
2023-02-21 20:15:14 -0300

    update code

--- umf_pa_interactive_map.R
+++ umf_pa_interactive_map.R
@@ -2,6 +2,7 @@ library(dplyr, warn.conflicts = FALSE)
 library(sf, warn.conflicts = FALSE)
 library(tmap, warn.conflicts = FALSE)
 
+
 umf <- st_read(
         'D:/concessaoFlorestal/data/umf_concessao_pa/umf_concessao_pa.shp'
 ) %>%
