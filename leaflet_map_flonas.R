d91e39f3aabdcd8709dcdf48e578be2f39e59f15
Robson santos <rcflorestal@yahoo.com.br>
2023-02-21 20:15:47 -0300

    Flonas Map - leaflet version

---
+++ leaflet_map_flonas.R
@@ -0,0 +1,74 @@
+library(dplyr, warn.conflicts = FALSE)
+library(sf, warn.conflicts = FALSE)
+library(leaflet)
+
+
+# Forest Concession in Pará State
+conces_pa <- c('FLORESTA NACIONAL DE CAXIUANÂ', 'FLORESTA NACIONAL DO CREPORI',
+               'FLORESTA NACIONAL DE SARACÁ-TAQUERA', 
+               'FLORESTA NACIONAL DE ALTAMIRA')
+
+# Load shapefile data
+flonas <- st_read(
+        'D:/geo/icmbio/UC_fed_junho_2020/flonas.shp',
+        options = 'ENCONDING=latin1'
+) %>%
+        filter(UF == 'PA') %>%
+        select(nome, areaHa, anoCriacao, areaHa, atoLegal, UF, municipios)
+
+flonas_conces <- flonas %>%
+        filter(nome %in% conces_pa)
+
+upas <- st_read(
+        'D:/concessaoFlorestal/data/upas_concessao_pa_20221206/upas_concessao_pa_20221206.shp'
+)
+
+umf <- st_read(
+        'D:/concessaoFlorestal/data/umf_concessao_pa/umf_concessao_pa.shp'
+)
+
+pop_flona <- paste0(
+        "", "<b>",flonas_conces$nom,"</b>",
+        "<br>",
+        "Hectares: ", prettyNum(flonas_conces$areaHa, big.mark = ".", decimal.mark = ","),
+        "<br>",
+        "Municípios: ", flonas_conces$municipios,
+        "<br>",
+        "Ano Criação: ", flonas_conces$anoCriacao
+)
+
+pop_umf <- paste0(
+        "", "<b>", umf$umf, "</b>",
+        "<br>",
+        "Hectares: ", prettyNum(umf$hectares, big.mark = ".", decimal.mark = ","),
+        "<br>",
+        "Concessionária: ", umf$conces,
+        "<br>",
+        "Situação: ", umf$status
+)
+
+flona_map <- flonas_conces %>%
+        leaflet() %>%
+        addTiles(group = "OpenStreetmap") %>%
+        addPolygons(
+                group = "Concessão",
+                fillOpacity = 0.05,
+                color = 'red',
+                weight = 3,
+                popup = pop_flona
+        ) %>%
+        addPolygons(
+                data = umf,
+                group = "UMF",
+                fillOpacity = 0.2,
+                color = "steelblue",
+                weight = 1,
+                popup = pop_umf
+        ) %>%
+        addLayersControl(
+                baseGroups = c("OpenStreetMap", "UMF"),
+                overlayGroups = c("Concessão"),
+                options = layersControlOptions(collapsed = FALSE)
+        ) %>%
+        addMiniMap(toggleDisplay = TRUE) %>%
+        leafem::addMouseCoordinates()
