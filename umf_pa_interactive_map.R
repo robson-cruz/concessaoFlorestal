72fc73d02ee409f12e624811aace14434150e0c2
Robson santos <rcflorestal@yahoo.com.br>
2023-02-21 20:14:32 -0300

    update code

--- umf_pa_interactive_map.R
+++ umf_pa_interactive_map.R
@@ -18,15 +18,22 @@ umf_map <- tm_shape(umf) +
                     id = 'umf',
                     popup.var = c('hectares',
                                   'Flona' = 'flona', 
-                                  'Concessionária' = 'conces', 
+                                  'Concessionária ' = 'conces', 
                                   'Situação' = 'status'),
-                    popup.format = list(hectares = list(digits = 2, decimal.mark = ',', big.mark = '.'))
+                    popup.format = list(hectares = list(digits = 2, 
+                                                        decimal.mark = ',',
+                                                        big.mark = '.'))
         ) +
         
         #tm_text('Name', size = 1.4) +
         
-        tm_facets(sync = FALSE, ncol = 2, by = 'flona', free.coords	
-                  = TRUE) +
+        tm_facets(
+                sync = FALSE, 
+                ncol = 2,
+                by = 'flona',
+                free.coords = TRUE,
+                as.layers = TRUE
+        ) +
         
         tm_basemap(server = 'OpenStreetMap') +
         tm_mouse_coordinates()
