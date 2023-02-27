5caecb23318bc4a2048acef1c7fc8c83221b1856
Robson Cruz <rcflorestal@yahoo.com.br>
2023-02-27 19:31:16 -0300

    Change DATUM to wgs84

--- leaflet_map_umf.R
+++ leaflet_map_umf.R
@@ -5,17 +5,20 @@ library(leaflet, warn.conflicts = FALSE)
 
 source("D:/concessaoFlorestal/leaflet_map_flonas.R")
 
-pa_boundary <- st_read('D:/geo/ibge/malha_municipal/PA_UF_2020.shp')
+pa_boundary <- st_read('D:/geo/ibge/malha_municipal/PA_UF_2020.shp') %>%
+        st_transform(4326)
 
 umf <- st_read(
         "D:/concessaoFlorestal/data/umf_concessao_pa/umf_concessao_pa.shp"
 ) %>%
-        mutate(hectares = as.numeric(hectares))
+        mutate(hectares = as.numeric(hectares)) %>%
+        st_transform(4326)
 
 upas <- st_read(
         "D:/concessaoFlorestal/data/upas_concessao_pa_20221206/upas_concessao_pa_20221206.shp"
 ) %>%
-        mutate(status = recode(status, "Exploracao Futura" = "Exploração Futura"))
+        mutate(status = recode(status, "Exploracao Futura" = "Exploração Futura")) %>%
+        st_transform(4326)
 
 pop_upa <- paste0(
         "<b>", "UPA ", "</b", "<b>", upas$UPA, "</b>",
@@ -41,7 +44,6 @@ pal <- colorFactor(palette = "RdYlBu", domain = upas$status)
 
 umf_map <- umf %>%
         leaflet() %>%
-        addTiles() %>%
         addProviderTiles(provider = "OpenStreetMap.Mapnik", group = "OpenStreetMap") %>%
         addProviderTiles(provider = "OpenTopoMap", group = "OpenTopoMap") %>%
         addPolygons(
